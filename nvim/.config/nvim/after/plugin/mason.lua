1366498578964dd88920f2e27925542a2f79d87d
Andreas Knudsen Alstrup <aalstr20@student.aau.dk>
2024-10-16 22:42:12 +0200

    Updated LSP setup

--- nvim/.config/nvim/after/plugin/mason.lua
+++ nvim/.config/nvim/after/plugin/mason.lua
@@ -2,7 +2,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
         ensure_installed = { 
                 'lua_ls',
-                'rust_analyzer', 
+                'rust_analyzer',
                 'eslint',
                 'pyright',
                 'gopls',
