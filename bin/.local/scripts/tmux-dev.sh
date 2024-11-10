4f7d8c1a014e68c59cef1d80e4eb5d2ceeb41839
Andreas Knudsen Alstrup <aalstr20@student.aau.dk>
2024-11-10 14:12:38 +0100

    Use arg to set dev env

--- bin/.local/scripts/tmux-dev.sh
+++ bin/.local/scripts/tmux-dev.sh
@@ -1,5 +1,7 @@
-tmux new-session -d -s "dev" -c ~/dev nvim .
+echo "dev environment set: $1"
+
+tmux new-session -d -s "dev" -c $1 nvim .
 tmux rename-window -t "dev" "nvim"
-tmux new-window -n "terminal" -c ~/dev
+tmux new-window -n "terminal" -c $1
 
 tmux attach-session -t "dev:0"
