c56b960e457012956e0357510cbb6ff1a66967a9
Andreas Knudsen Alstrup <aalstr20@student.aau.dk>
2024-11-18 23:36:45 +0100

    Added trouble plugin

---
+++ nvim/.config/nvim/after/plugin/trouble.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
+
+vim.keymap.set("n", "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>", { desc = "Symbols (Trouble)" })
--- nvim/.config/nvim/lua/novem/packer.lua
+++ nvim/.config/nvim/lua/novem/packer.lua
@@ -4,73 +4,71 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+        -- Packer can manage itself
+        use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  
-  use({
-	  "rose-pine/neovim", 
-	  as = "rose-pine",
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+        use {
+                'nvim-telescope/telescope.nvim', tag = '0.1.8',
+                -- or                            , branch = '0.1.x',
+                requires = { { 'nvim-lua/plenary.nvim' } }
+        }
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use('lewis6991/gitsigns.nvim')
+        use({
+                "rose-pine/neovim",
+                as = "rose-pine",
+                config = function()
+                        vim.cmd('colorscheme rose-pine')
+                end
+        })
 
-  use({
-     'numToStr/Comment.nvim',
-      config = function()
-      require('Comment').setup()
-     end
-  })
+        use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+        use('mbbill/undotree')
+        use('tpope/vim-fugitive')
+        use('lewis6991/gitsigns.nvim')
+        use({
+                'folke/trouble.nvim',
+                config = function()
+                        require("trouble").setup {
+                        }
+                end
+        })
 
-  use {"akinsho/toggleterm.nvim", tag = '*', config = function()
-          require("toggleterm").setup()
-  end}
+        use({
+                'numToStr/Comment.nvim',
+                config = function()
+                        require('Comment').setup()
+                end
+        })
 
-  -- lsp-zero
-  --use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
-  --use({'neovim/nvim-lspconfig'})
-  --use({'hrsh7th/nvim-cmp'})
-  --use({'hrsh7th/cmp-nvim-lsp'})
+        use { "akinsho/toggleterm.nvim", tag = '*', config = function()
+                require("toggleterm").setup()
+        end }
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim', branch = 'v4.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+        -- lsp-zero
+        --use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
+        --use({'neovim/nvim-lspconfig'})
+        --use({'hrsh7th/nvim-cmp'})
+        --use({'hrsh7th/cmp-nvim-lsp'})
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+        use {
+                'VonHeikemen/lsp-zero.nvim', branch = 'v4.x',
+                requires = {
+                        -- LSP Support
+                        { 'neovim/nvim-lspconfig' },
+                        { 'williamboman/mason.nvim' },
+                        { 'williamboman/mason-lspconfig.nvim' },
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }	  
+                        -- Autocompletion
+                        { 'hrsh7th/nvim-cmp' },
+                        { 'hrsh7th/cmp-buffer' },
+                        { 'hrsh7th/cmp-path' },
+                        { 'saadparwaiz1/cmp_luasnip' },
+                        { 'hrsh7th/cmp-nvim-lsp' },
+                        { 'hrsh7th/cmp-nvim-lua' },
+
+                        -- Snippets
+                        { 'L3MON4D3/LuaSnip' },
+                        { 'rafamadriz/friendly-snippets' },
+                }
+        }
 end);
-
-
-
-
-
-
-
-
-
