f0ae2cb14a4b7b45a56ca1a1a6d00ecbfe1fd244
Andreas Knudsen Alstrup <aalstr20@student.aau.dk>
2024-11-25 23:37:46 +0100

    Added buffer commands

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,27 @@
+local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
+
+require('telescope').setup({
+        defaults = {
+        },
+        pickers = {
+                buffers = {
+                        mappings = {
+                                i = {
+                                        ['<C-d>'] = actions.delete_buffer,
+                                },
+                                n = {
+                                        ['<C-d>'] = actions.delete_buffer,
+                                },
+                        },
+                },
+        },
+})
+
 -- project find
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find files in git only' })
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+        builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-
+vim.keymap.set('n', '<leader>o', builtin.buffers, { desc = 'Buffers' })
