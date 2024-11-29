f3727ccf7d3bd153a4842aabe89327023e8ac52e
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2024-11-29 16:18:36 +0100

    Change tmux base index to 1

--- bin/.local/scripts/tmux-dev.sh
+++ bin/.local/scripts/tmux-dev.sh
@@ -1,7 +1,5 @@
-echo "dev environment set: $1"
-
 tmux new-session -d -s "dev" -c $1 nvim .
 tmux rename-window -t "dev" "nvim"
 tmux new-window -n "terminal" -c $1
 
-tmux attach-session -t "dev:0"
+tmux attach-session -t "dev:1"
