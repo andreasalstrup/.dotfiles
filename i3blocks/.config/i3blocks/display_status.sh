478253448b1aae325085af78cde9cc2d11fb5c15
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2025-04-12 18:11:54 +0200

    Store current display position

--- i3blocks/.config/i3blocks/display_status.sh
+++ i3blocks/.config/i3blocks/display_status.sh
@@ -10,11 +10,21 @@ then
     exit 0
 fi
 
-OUTPUT="display>"
+# Read stored position
+POSITION=$(cat /var/tmp/i3blocks/displays  2>/dev/null)
+case "$POSITION" in
+    left) OUTPUT="<display" ;;
+    right) OUTPUT="display>" ;;
+esac
+
+# Update
 case "$BLOCK_BUTTON" in
     1) POSITION=left; OUTPUT="<display" ;;
     3) POSITION=right; OUTPUT="display>" ;;
 esac
+
+# Store current position
+mkdir -p /var/tmp/i3blocks && echo $POSITION > /var/tmp/i3blocks/displays
 
 ~/.local/scripts/xrandr_displays.sh $POSITION
 
