f13066d517c567ea9f6f87ab748c61b2734209cb
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2025-06-09 19:00:36 +0200

    Style lualine

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -1,7 +1,12 @@
 require('lualine').setup({
         sections = {
+                lualine_a = {},
+                -- lualine_b = {},
                 lualine_c = {
                         'lsp_progress'
-                }
+                },
+                lualine_x = {},
+                -- lualine_y = {},
+                lualine_z = {},
         }
 })
