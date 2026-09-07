4e65a85d1d5e4950bbd775b4e2c9ecb36d61b314
andreasalstrup <andreas-contact@pm.me>
2026-09-07 20:44:13 +0200

    Replace mason when using nixos

--- nixos/modules/home-manager/nvim.nix
+++ nixos/modules/home-manager/nvim.nix
@@ -1,10 +1,33 @@
 { pkgs, config, ... }:
 
 {
-  home.packages = [ pkgs.neovim ];
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    extraPackages = with pkgs; [
+      # LSP Servers
+      gopls
+      rust-analyzer
+      typescript-language-server
+      lua-language-server
+      clang-tools
+      pyright
+      nil
+
+      # Formatters/Linters
+      stylua
+      black
+      eslint_d
+    ];
+  };
 
   home.file.".config/nvim" = {
     source = ../../../nvim/.config/nvim;
     recursive = true;
   };
+
+  # Install packer.nvim itself so `require('packer')` resolves
+  # home.file.".local/share/nvim/site/pack/packer/start/packer.nvim" = {
+  #   source = pkgs.vimPlugins.packer-nvim;
+  # };
 }
